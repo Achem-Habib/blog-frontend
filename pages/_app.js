c783ab654430fae1db6441e9a10afe6c028ba734
Achem-Habib <achemnomaer57@gmail.com>
2023-04-13 11:53:42 +0600

    ahrefs verification

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,10 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta
+          name="ahrefs-site-verification"
+          content="211fd062142ff2c6a14ccff9e3f3de680f351ebb9eb46d77d75f954edbdde419"
+        />
       </Head>
 
       <LayoutWrapper>
